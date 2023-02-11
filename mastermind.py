9178ce2991c30d8591a2dc02fb3a46e433c9bbc9
Randhawa Verma <randhawaverma@gmail.com>
2023-02-11 12:09:51 +0530

    Adding project file to git

---
+++ mastermind.py
@@ -0,0 +1,9 @@
+#1. Generate 4 color random code
+import random
+
+COLORS = ['R','G','B','Y','W','O']
+TRIES = 10
+
+#2. Make the user guess the code
+#3. Compare the guess
+#4. Tie the game together
